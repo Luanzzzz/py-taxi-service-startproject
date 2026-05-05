eeb9097e29e1c9b6402dfb4de7196f7b7c29b277
Luan <luan.valentino78@gmail.com>
2026-05-05 16:08:31 -0300

    Dev

---
+++ taxi_app/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+# Create your models here.
+
+
+class Manufacturer(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    country = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+class Driver(AbstractUser):
+    license_number = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return f"{self.username} ({self.first_name} {self.last_name})"
+
+class Car(models.Model):
+    model = models.CharField(max_length=255, unique=True)
+    manufacture = models.ForeignKey(Manufacturer, on_delete=models.CASCADE, related_name="cars")
+    drivers= models.ManyToManyField(Driver, related_name="cars")
+
+    def __str__(self):
+        return self.model
